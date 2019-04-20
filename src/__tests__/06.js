9175c295c9bbd5823ef536046092622cd6eb7d5e
Arnold Gandarillas Castillo <arkgast@gmail.com>
2019-04-20 18:33:22 -0500

    email autofill

--- src/__tests__/06.js
+++ src/__tests__/06.js
@@ -41,7 +41,7 @@ test('passes custom props to the custom-button', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=react%20hooks%20patterns&e=06&em=
+http://ws.kcd.im/?ws=react%20hooks%20patterns&e=06&em=arkgast%40gmail.com
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
